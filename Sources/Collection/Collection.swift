65dc0a758f3dcf4836ea5e92c3d4d20bb22e7aaf
Jakub Kiermasz <kiermasz.k@gmail.com>
2021-05-13 13:39:46 +0200

    Add safe area insets padding to collection content

--- Sources/Collection/Collection.swift
+++ Sources/Collection/Collection.swift
@@ -5,32 +5,38 @@
 import SwiftUI
 
 public struct Collection: View {
-    
+
     // MARK: - Properties
-    
+
     private let sections: [AnySection]
     private let registry: Registry
-    
+
     public var showsIndicators: Bool
 
     // MARK: - Getters
-    
+
     public var body: some View {
-        ScrollView(showsIndicators: showsIndicators) {
-            LazyVStack {
-                ForEach(sections, id: \.self) { section in
-                    registry.view(for: section)
+        GeometryReader { geometryWithSafeArea in
+            GeometryReader { _ in
+                ScrollView(showsIndicators: showsIndicators) {
+                    LazyVStack {
+                        ForEach(sections, id: \.self) { section in
+                            registry.view(for: section)
+                        }
+                    }
+                    .padding(geometryWithSafeArea.safeAreaInsets)
                 }
             }
+            .edgesIgnoringSafeArea(.all)
         }
     }
 
     // MARK: - Initialization
-    
+
     public init(showsIndicators: Bool = true, dataSource: CollectionDataSource, @CollectionBuilder builder: () -> Registry) {
         self.showsIndicators = showsIndicators
         registry = builder()
         sections = dataSource.sections
     }
-    
+
 }
